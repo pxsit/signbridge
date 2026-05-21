dec3581826658abcbc0871a15c9ca65f779dadea
Pasit Sangprachathanarak <ouipingpasit@gmail.com>
2026-05-21 16:58:44 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/signbridge/sw.js', { scope: '/signbridge/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
